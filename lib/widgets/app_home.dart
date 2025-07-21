488d3d78f38b3a53f35e55d7e4ee104593016a11
Ecky819 <marcoeggert73@gmail.com>
2025-07-21 13:05:16 +0200

    AUfgabe 2

---
+++ lib/widgets/app_home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '/news_screen.dart';
+import '/likes_screen.dart';
+import '/profile_screen.dart';
+
+class AppHome extends StatefulWidget {
+  const AppHome({super.key});
+
+  @override
+  State<AppHome> createState() => _AppHomeState();
+}
+
+class _AppHomeState extends State<AppHome> {
+  int currentIndex = 0;
+
+  final List<Widget> screens = const [
+    NewsScreen(),
+    LikesScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('AppHome')),
+      body: screens[currentIndex],
+    );
+  }
+}
